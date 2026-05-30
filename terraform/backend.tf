3c20317945c4674112229f965f0bd8277a5e4a45
omid <omidsarafmanesh.osm@outlook.com>
2026-05-30 20:12:44 +1000

    Terraform backend lock

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    # These values will be set by deployment scripts
-    # For local development, they can be passed via -backend-config
+    use_lockfile   = true
   }
 }
